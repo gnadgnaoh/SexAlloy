9881a0be007f741ec2afeba01d55e90509bbe3d7
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-09-11 05:17:29 +0700

    Update ProtonVpnHook.kt

--- app/src/main/java/io/github/nexalloy/hoodles/morphe/protonvpn/ProtonVpnHook.kt
+++ app/src/main/java/io/github/nexalloy/hoodles/morphe/protonvpn/ProtonVpnHook.kt
@@ -1,9 +1,11 @@
 package io.github.nexalloy.hoodles.morphe.protonvpn
 
 import io.github.nexalloy.hoodles.morphe.protonvpn.delay.RemoveChangeServerDelay
+import io.github.nexalloy.hoodles.morphe.protonvpn.premium.UnlockVpnPlus
 import io.github.nexalloy.hoodles.morphe.protonvpn.telemetry.DisableTelemetry
 
 val ProtonVpnPatches = arrayOf(
+    UnlockVpnPlus,
     RemoveChangeServerDelay,
     DisableTelemetry,
 )
