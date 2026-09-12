e8a77ddf0b548ee700bb0dba601de0b67a09ae00
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-09-12 11:06:06 +0700

    Update TikTokHook.kt

--- app/src/main/java/io/github/nexalloy/morphe/tiktok/TikTokHook.kt
+++ app/src/main/java/io/github/nexalloy/morphe/tiktok/TikTokHook.kt
@@ -3,11 +3,30 @@ package io.github.nexalloy.morphe.tiktok
 import io.github.nexalloy.Patch
 import io.github.nexalloy.morphe.tiktok.ads.HidePromotedMusicVideos
 import io.github.nexalloy.morphe.tiktok.ads.RemoveFeedAds
+import io.github.nexalloy.morphe.tiktok.captcha.HideCaptchaPopups
+import io.github.nexalloy.morphe.tiktok.login.DisableLoginRequirement
+import io.github.nexalloy.morphe.tiktok.login.FixGoogleLogin
+import io.github.nexalloy.morphe.tiktok.screencapture.DisableScreenCaptureDetection
+import io.github.nexalloy.morphe.tiktok.watermark.RemoveDownloadWatermark
 
 const val TIKTOK_PACKAGE_NAME = "com.zhiliaoapp.musically"
 const val TIKTOK_ASIA_PACKAGE_NAME = "com.ss.android.ugc.trill"
 
 val TikTokPatches = arrayOf<Patch>(
+    // Ads
     RemoveFeedAds,
     HidePromotedMusicVideos,
+
+    // Privacy
+    DisableScreenCaptureDetection,
+
+    // Account
+    DisableLoginRequirement,
+    FixGoogleLogin,
+
+    // Interruptions
+    HideCaptchaPopups,
+
+    // Downloads
+    RemoveDownloadWatermark,
 )
